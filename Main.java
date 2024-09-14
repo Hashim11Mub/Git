5e499eb368c281650abd762a3c5703d0b93eae9c
Hashem Almubarak <105522749+Hashim11Mub@users.noreply.github.com>
2024-09-14 18:19:10 +0300

    Add readStudents method

--- Main.java
+++ Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Initial Setup");
     }
+
+    public static void readStudents() {
+        System.out.println("Reading list of students");
+    }
 }
